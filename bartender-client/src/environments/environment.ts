7f4e7d3d50e4380eb224fc60e6184c4c39030b5e
brandon <bs.edwards22@gmail.com>
2026-02-27 21:39:55 -0600

    config updates

--- bartender-client/src/environments/environment.ts
+++ bartender-client/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   // apiUrl: 'https://localhost:44389'   // your dev API
-  apiUrl: 'http://localhost:5000'   // your dev API
+  // apiUrl: 'http://localhost:5000'   // your dev API
+  apiUrl: 'http://192.168.0.64:5000'   // your dev API
 };
